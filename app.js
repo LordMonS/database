cebc83e17ddc5b8561d2c4c31fc6434064a77772
LordMonS <williamkosasie3@gmail.com>
2024-03-09 16:54:57 +0700

    bbisalah

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const db = mysql.createConnection({
     host:'database-1-database-001.a.aivencloud.com',
     password:'AVNS_Ro1Fi0uxIFN_I_AWzpX',
     database:'defaultdb',
+    port:17004
 });
 
 app.use((req, res, next)=>{
@@ -28,7 +29,7 @@ app.post('/save',(req,res)=>{
         if(results.length<=0){
             db.query('INSERT INTO user (user) VALUES (?)',[user], (err, r)=>{
                 if(err){
-                    console.lof(err)
+                    console.log(err)
                 }else{
                     console.log(r.insertId)
                     res.send({message:"berhasil"})
